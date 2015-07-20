862c08c8c0d3ef9b1391234acd7bf616f41654d1
Alexander Wu <hgalexwu@hotmail.com>
2015-07-19 23:15:58 -0400

    Initial Commit
    Flourish app, basically a diary, credits to Thinkster.io. Followed his
    tutorial. Finished designing the entry view interface.

---
+++ Flourish/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flourish
+//
+//  Created by Alexander Wu on 2015-07-18.
+//  Copyright (c) 2015 Alexander Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Flourish/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Flourish
+//
+//  Created by Alexander Wu on 2015-07-18.
+//  Copyright (c) 2015 Alexander Wu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
